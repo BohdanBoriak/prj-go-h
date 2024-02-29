1ffb9bdf6070b9370f070c0b1fc5ce75d14fe21d
BohdanBoriak <lightraven72@gmial.com>
2024-02-29 15:40:49 +0200

    added User struct

--- main.go
+++ main.go
@@ -3,37 +3,68 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"prj-go-h/domain"
 	"strconv"
 	"time"
 )
 
-var rohalyky int = 50
+const (
+	rohalyky            = 50
+	rohalykyPerQuestion = 5
+)
+
+var id uint64 = 1
 
 func main() {
 	fmt.Println("Вітаємо у грі \"РОГАЛИКИ\"")
 	time.Sleep(1 * time.Second)
+
+	for {
+		menu()
+		punct := ""
+		fmt.Scan(&punct)
+
+		switch punct {
+		case "1":
+			play()
+		case "2":
+			fmt.Println("Рейтинг ще не завезли!")
+		case "3":
+			return
+		default:
+			fmt.Println("Та що ви таке ввели!")
+		}
+	}
+}
+
+func menu() {
+	fmt.Println("Виберіть пункт меню:")
+	fmt.Println("1. Грати в рогалики!")
+	fmt.Println("2. Рейтинг гравців...")
+	fmt.Println("3. Залишити гру =(")
+}
+
+func play() domain.User {
 	fmt.Println("Відповідай і вигравай!")
 
-	rohalykyPerQuestion := 5
 	moiiRohalyky := 0
+	totalRohalyky := rohalyky
 	now := time.Now()
 
-	for rohalyky > 0 {
+	for totalRohalyky > 0 {
 		x, y := rand.Intn(100), rand.Intn(100)
 		res := x + y
-
 		fmt.Printf("%v + %v = ?\n", x, y)
+
 		var ans string
-
 		fmt.Scan(&ans)
-
 		ansInt, err := strconv.Atoi(ans)
 		if err != nil {
 			fmt.Printf("error: %s\n", err)
 		} else {
 			if ansInt == res {
 				moiiRohalyky += rohalykyPerQuestion
-				rohalyky -= rohalykyPerQuestion
+				totalRohalyky -= rohalykyPerQuestion
 				fmt.Printf("Чудово, у тебе: %v рогаликів\n", moiiRohalyky)
 			} else {
 				fmt.Println("Яке розчарування... Спробуй ще!")
@@ -41,6 +72,25 @@ func main() {
 		}
 	}
 	then := time.Now()
-	fmt.Printf("ЧУДОВО! Ти зібрав усі рогалики за %v секунд!", then.Sub(now))
-	time.Sleep(10 * time.Second)
+	timeSpent := then.Sub(now)
+
+	fmt.Printf("ЧУДОВО! Ти зібрав усі рогалики за %v секунд!", timeSpent)
+
+	fmt.Println("Введіть ім'я: ")
+	name := ""
+	fmt.Scan(&name)
+
+	user := domain.User{
+		Id:   id,
+		Name: name,
+		Time: timeSpent,
+	}
+	id++
+
+	// var user domain.User
+	// user.Id = id
+	// user.Name = name
+	// user.Time = timeSpent
+
+	return user
 }
